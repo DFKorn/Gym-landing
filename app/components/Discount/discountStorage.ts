7412eefa747724a47f00452cf6766db6696a0daf
DFKorn <d1man2192@mail.ru>
2026-02-09 23:21:01 +0300

    change timer to 1 min

--- app/components/Discount/discountStorage.ts
+++ app/components/Discount/discountStorage.ts
@@ -1,6 +1,6 @@
 export const STORAGE_KEY = "discount_expire_at";
 
-export function getOrCreateExpireAt(DURATION = 1 * 20 * 1000) {
+export function getOrCreateExpireAt(DURATION = 1 * 60 * 1000) {
   let saved = localStorage.getItem(STORAGE_KEY);
 
   if (!saved) {
